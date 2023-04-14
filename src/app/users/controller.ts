375a4c3df27cec74f54eed08dadfdb985b9e6b5e
Aziz <med.aziz.gharbii@gmail.com>
2023-04-14 13:28:52 -0400

    fix: pre-commit sucks

--- src/app/users/controller.ts
+++ src/app/users/controller.ts
@@ -5,46 +5,46 @@ import userService from "./service";
 @Tags("User")
 @Route("/api/v1/user")
 export class UserController {
-  /*
-   * User COntroller
-   */
-  @Post("/register")
-  async register(@Body() requestBody: RegisterRequest) {
-    try {
-      const user = await userService.register(requestBody);
-      return user;
-    } catch (error: any) {
-      throw new Error(error);
-    }
-  }
+	/*
+	 * User COntroller
+	 */
+	@Post("/register")
+	async register(@Body() requestBody: RegisterRequest) {
+		try {
+			const user = await userService.register(requestBody);
+			return user;
+		} catch (error: any) {
+			throw new Error(error);
+		}
+	}
 
-  @Post("/login")
-  async login(@Body() requesBody: LogionRequest) {
-    try {
-      const token = await userService.login(requesBody);
-      return token;
-    } catch (error: any) {
-      throw new Error(error);
-    }
-  }
+	@Post("/login")
+	async login(@Body() requesBody: LogionRequest) {
+		try {
+			const token = await userService.login(requesBody);
+			return token;
+		} catch (error: any) {
+			throw new Error(error);
+		}
+	}
 
-  @Security("jwt")
+	@Security("jwt")
   @Get("/secret")
-  async secret() {
-    try {
-      return { message: "You have access to this ressorces" };
-    } catch (error: any) {
-      throw new Error(error);
-    }
-  }
+	async secret() {
+		try {
+			return { message: "You have access to this ressorces" };
+		} catch (error: any) {
+			throw new Error(error);
+		}
+	}
 
-  @Get()
-  async getAllUsers() {
-    try {
-      const users = await userService.getAllusers();
-      return users;
-    } catch (error: any) {
-      throw new Error(error);
-    }
-  }
+	@Get()
+	async getAllUsers() {
+		try {
+			const users = await userService.getAllusers();
+			return users;
+		} catch (error: any) {
+			throw new Error(error);
+		}
+	}
 }
